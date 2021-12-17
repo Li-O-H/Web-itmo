e80dae0ccf9d73f8115b939c795ee12393bc8c14
Leon <leon25022002@gmail.com>
2021-12-17 22:16:24 +0300

    Now localstorage works

--- Scripts/create_deadlines.js
+++ Scripts/create_deadlines.js
@@ -6,8 +6,10 @@ window.addEventListener("load", function () {
             this.date = date;
         }
     }
-    let deadlines=[]
-    // deadlines = JSON.parse(localStorage.getItem("deadlines"))
+    let deadlines = JSON.parse(localStorage.getItem("deadlines"))
+    if (deadlines == null) {
+        deadlines = []
+    }
     displayDeadlines()
     let taskInput = document.getElementById("task_input");
     let dateInput = document.getElementById("date_input");
@@ -16,11 +18,11 @@ window.addEventListener("load", function () {
         function (event) {
             event.preventDefault();
             let task = taskInput.value
-            taskInput.value=''
+            taskInput.value = ''
             let date = dateInput.value
-            dateInput.value=''
+            dateInput.value = ''
             deadlines.push(new Deadline(task, date));
-            // localStorage.setItem("deadlines", JSON.stringify(deadlines))
+            localStorage.setItem("deadlines", JSON.stringify(deadlines))
             displayDeadlines()
         }
     )
@@ -47,21 +49,21 @@ window.addEventListener("load", function () {
         while (container.firstChild) {
             container.removeChild(container.firstChild);
         }
-        deadlines.forEach(function (deadline, i, deadlines){
+        deadlines.forEach(function (deadline, i, deadlines) {
             const newElem = document.createElement("div");
             const leftSide = document.createElement("div");
             const rightSide = document.createElement("div");
             newElem.appendChild(leftSide)
             newElem.appendChild(rightSide)
-            const number=document.createElement("p")
-            number.innerHTML=i+1
-            const task=document.createElement("p")
-            task.innerHTML=deadline.task
-            const date=document.createElement("p")
-            date.innerHTML=deadline.date
-            const deleteButton=document.createElement("button")
+            const number = document.createElement("p")
+            number.innerHTML = i + 1
+            const task = document.createElement("p")
+            task.innerHTML = deadline.task
+            const date = document.createElement("p")
+            date.innerHTML = deadline.date
+            const deleteButton = document.createElement("button")
             deleteButton.classList.add("deadline_delete_button")
-            deleteButton.innerHTML="X"
+            deleteButton.innerHTML = "X"
             leftSide.appendChild(number)
             leftSide.appendChild(task)
             rightSide.appendChild(date)
@@ -71,9 +73,9 @@ window.addEventListener("load", function () {
             task.classList.add("deadline_task")
             date.classList.add("deadline_date")
             newElem.classList.add("deadline")
-            deleteButton.addEventListener("click", function (){
-                delete deadlines[i];
-                // localStorage.setItem("deadlines", JSON.stringify(deadlines))
+            deleteButton.addEventListener("click", function () {
+                deadlines.splice(i, 1);
+                localStorage.setItem("deadlines", JSON.stringify(deadlines))
                 displayDeadlines()
             })
             container.appendChild(newElem)
