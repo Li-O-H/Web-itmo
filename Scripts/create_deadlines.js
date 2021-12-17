61cf33a6642ad4af7f797cd387cb81ae61a2b055
Leon <leon25022002@gmail.com>
2021-12-17 16:32:22 +0300

    Basic functionality

---
+++ Scripts/create_deadlines.js
@@ -0,0 +1,54 @@
+window.addEventListener("load", function () {
+    const container = document.getElementById('deadlines');
+    class Deadline {
+        constructor(task, date) {
+            this.task = task;
+            this.date = date;
+        }
+    }
+
+    let deadlines = []
+    let taskInput = document.getElementById("task_input");
+    let dateInput = document.getElementById("date_input");
+    let submit = document.getElementById("submit");
+    submit.addEventListener("click",
+        function (event) {
+            event.preventDefault();
+            let task = taskInput.value
+            taskInput.value=''
+            dateInput.value=''
+            let date = dateInput.value
+            deadlines.push(new Deadline(task, date));
+            displayDeadlines()
+        }
+    )
+
+    function displayDeadlines() {
+        deadlines.sort(function (a, b) {
+            if ((a.date !== '') && (b.date !== '')) {
+                if (a.date < b.date) {
+                    return -1
+                }
+                if (a.date > b.date) {
+                    return 1
+                }
+                return a.task.localeCompare(b.task)
+            }
+            if (a.date !== '') {
+                return -1
+            }
+            if (b.date !== '') {
+                return 1
+            }
+            return a.task.localeCompare(b.task)
+        })
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        deadlines.forEach(function (deadline, i, deadlines){
+            const newElem = document.createElement("p");
+            newElem.innerHTML=deadline.task + deadline.date.toString();
+            container.appendChild(newElem)
+        });
+    }
+})
