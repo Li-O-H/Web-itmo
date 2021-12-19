084b28bb4aaaefc1c26816052c10d7c353d44062
Leon <leon25022002@gmail.com>
2021-12-19 15:26:40 +0300

    Basic functionality

---
+++ Scripts/load_comments.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", async function loadComments() {
+    const container = document.getElementById('comments');
+    let comments = []
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/comments')
+        comments = await response.json();
+    } catch (e) {
+        const message = document.createElement("p")
+        message.innerHTML = "Ошибка - не удалось загрузить комментарии"
+        container.appendChild(message)
+        return
+    }
+
+    function getRandomInRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    comments = comments.filter(function (item, index, array) {
+        return (item.id % getRandomInRange(90, 200) === 0);
+    });
+    comments.forEach(function (comment, i, comments) {
+        const newElem = document.createElement("article");
+        const authorEmail = document.createElement("a")
+        authorEmail.href = "mailto: " + comment.email
+        authorEmail.innerHTML = comment.email
+        const author = document.createElement("p")
+        author.innerHTML = comment.name + ", " + authorEmail.outerHTML
+        const body = document.createElement("p")
+        body.innerHTML = comment.body
+        newElem.appendChild(author)
+        newElem.appendChild(body)
+        container.appendChild(newElem)
+    });
+})
